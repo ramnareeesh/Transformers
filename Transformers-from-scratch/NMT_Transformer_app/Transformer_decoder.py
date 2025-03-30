f3630e90961f35bcfc90ab6acabd24c8f1b09785
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2025-03-30 09:50:08 +0530

    Starting the transformer app, split the various modules from the Final transformer

---
+++ Transformers-from-scratch/NMT_Transformer_app/Transformer_decoder.py
@@ -0,0 +1,187 @@
+import tensorflow as tf
+import random
+import numpy as np
+from keras.layers import LayerNormalization, Layer, Dense, ReLU, Dropout,TextVectorization, Embedding
+from keras.backend import softmax
+from tensorflow import math, matmul, reshape, shape, transpose, cast, float32
+
+
+class PositionEmbeddingFixedWeights(Layer):
+    def __init__(self, seq_length, vocab_size, output_dim, **kwargs):
+        super().__init__(**kwargs)
+        word_embedding_matrix = self.get_position_encoding(vocab_size, output_dim)
+        pos_embedding_matrix = self.get_position_encoding(seq_length, output_dim)
+        self.word_embedding_layer = Embedding(input_dim=vocab_size, output_dim=output_dim,
+                                              weights=[word_embedding_matrix], trainable=False)
+        self.position_embedding_layer = Embedding(input_dim=seq_length, output_dim=output_dim,
+                                                  weights=[pos_embedding_matrix], trainable=False)
+
+    def get_position_encoding(self, seq_len, d, n=10000):
+        P = np.zeros((seq_len, d))
+        for k in range(seq_len):
+            for i in np.arange(int(d / 2)):
+                denominator = np.power(n, 2 * i / d)
+                P[k, 2 * i] = np.sin(k / denominator)
+                P[k, 2 * i + 1] = np.cos(k / denominator)
+        return P
+
+    def call(self, inputs):
+        position_indices = tf.range(tf.shape(inputs)[-1])
+        embedded_words = self.word_embedding_layer(inputs)
+        embedded_indices = self.position_embedding_layer(position_indices)
+        return embedded_words + embedded_indices
+
+
+class DotProductAttention(Layer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def call(self, queries, keys, values, d_k, mask=None):
+        # Scoring the queries against the keys after transposing the latter, and scaling
+        scores = matmul(queries, keys, transpose_b=True) / math.sqrt(cast(d_k, float32))
+
+        # Apply mask to the attention scores
+        if mask is not None:
+            scores += -1e9 * mask
+
+        # Computing the weights by a softmax operation
+        weights = softmax(scores)
+
+        # Computing the attention by a weighted sum of the value vectors
+        return matmul(weights, values)
+
+
+class MultiHeadAttention(Layer):
+    def __init__(self, h, d_k, d_v, d_model, **kwargs):
+        super().__init__(**kwargs)
+        self.attention = DotProductAttention()  # Scaled dot product attention
+        self.heads = h  # Number of attention heads to use
+        self.d_k = d_k  # Dimensionality of the linearly projected queries and keys
+        self.d_v = d_v  # Dimensionality of the linearly projected values
+        self.d_model = d_model  # Dimensionality of the model
+        self.W_q = Dense(d_k)  # Learned projection matrix for the queries
+        self.W_k = Dense(d_k)  # Learned projection matrix for the keys
+        self.W_v = Dense(d_v)  # Learned projection matrix for the values
+        self.W_o = Dense(d_model)  # Learned projection matrix for the multi-head output
+
+    def reshape_tensor(self, x, heads, flag):
+        if flag:
+            # Tensor shape after reshaping and transposing:
+            # (batch_size, heads, seq_length, -1)
+            x = reshape(x, shape=(shape(x)[0], shape(x)[1], heads, -1))
+            x = transpose(x, perm=(0, 2, 1, 3))
+        else:
+            # Reverting the reshaping and transposing operations:
+            # (batch_size, seq_length, d_k)
+            x = transpose(x, perm=(0, 2, 1, 3))
+            x = reshape(x, shape=(shape(x)[0], shape(x)[1], self.d_k))
+        return x
+
+    def call(self, queries, keys, values, mask=None):
+        # Rearrange the queries to be able to compute all heads in parallel
+        q_reshaped = self.reshape_tensor(self.W_q(queries), self.heads, True)
+        # Resulting tensor shape: (batch_size, heads, input_seq_length, -1)
+        # Rearrange the keys to be able to compute all heads in parallel
+        k_reshaped = self.reshape_tensor(self.W_k(keys), self.heads, True)
+        # Resulting tensor shape: (batch_size, heads, input_seq_length, -1)
+        # Rearrange the values to be able to compute all heads in parallel
+        v_reshaped = self.reshape_tensor(self.W_v(values), self.heads, True)
+        # Resulting tensor shape: (batch_size, heads, input_seq_length, -1)
+        # Compute the multi-head attention output using the reshaped queries,
+        # keys, and values
+        o_reshaped = self.attention(q_reshaped, k_reshaped, v_reshaped, self.d_k, mask)
+        # Resulting tensor shape: (batch_size, heads, input_seq_length, -1)
+        # Rearrange back the output into concatenated form
+        output = self.reshape_tensor(o_reshaped, self.heads, False)
+        # Resulting tensor shape: (batch_size, input_seq_length, d_v)
+        # Apply one final linear projection to the output to generate the multi-head
+        # attention. Resulting tensor shape: (batch_size, input_seq_length, d_model)
+        return self.W_o(output)
+
+# Implementing the Add & Norm Layer
+class AddNormalization(Layer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.layer_norm = LayerNormalization() # Layer normalization layer
+    def call(self, x, sublayer_x):
+        # The sublayer input and output need to be of the same shape to be summed
+        add = x + sublayer_x
+        # Apply layer normalization to the sum
+        return self.layer_norm(add)
+
+
+# Implementing the Feed-Forward Layer
+class FeedForward(Layer):
+    def __init__(self, d_ff, d_model, **kwargs):
+        super().__init__(**kwargs)
+        self.fully_connected1 = Dense(d_ff)  # First fully connected layer
+        self.fully_connected2 = Dense(d_model)  # Second fully connected layer
+        self.activation = ReLU()  # ReLU activation layer
+
+    def call(self, x):
+        # The input is passed into the two fully-connected layers, with a ReLU in between
+        x_fc1 = self.fully_connected1(x)
+        return self.fully_connected2(self.activation(x_fc1))
+
+
+class DecoderLayer(Layer):
+    def __init__(self, h, d_k, d_v, d_model, d_ff, rate, **kwargs):
+        super().__init__(**kwargs)
+        self.multihead_attention1 = MultiHeadAttention(h, d_k, d_v, d_model)
+        self.dropout1 = Dropout(rate)
+        self.add_norm1 = AddNormalization()
+        self.multihead_attention2 = MultiHeadAttention(h, d_k, d_v, d_model)
+        self.dropout2 = Dropout(rate)
+        self.add_norm2 = AddNormalization()
+        self.feed_forward = FeedForward(d_ff, d_model)
+        self.dropout3 = Dropout(rate)
+        self.add_norm3 = AddNormalization()
+
+    def call(self, x, encoder_output, lookahead_mask, padding_mask, training):
+        # Multi-head attention layer
+        multihead_output1 = self.multihead_attention1(x, x, x, lookahead_mask)
+
+        # Expected output shape = (batch_size, sequence_length, d_model)
+        # Add in a dropout layer
+        multihead_output1 = self.dropout1(multihead_output1, training=training)
+        # Followed by an Add & Norm layer
+        addnorm_output1 = self.add_norm1(x, multihead_output1)
+        # Expected output shape = (batch_size, sequence_length, d_model)
+        # Followed by another multi-head attention layer
+        multihead_output2 = self.multihead_attention2(addnorm_output1, encoder_output, encoder_output, padding_mask)
+
+        # Add in another dropout layer
+        multihead_output2 = self.dropout2(multihead_output2, training=training)
+
+        # Followed by another Add & Norm layer
+        addnorm_output2 = self.add_norm1(addnorm_output1, multihead_output2)
+
+        # Followed by a fully connected layer
+        feedforward_output = self.feed_forward(addnorm_output2)
+        # Expected output shape = (batch_size, sequence_length, d_model)
+
+        # Add in another dropout layer
+        feedforward_output = self.dropout3(feedforward_output, training=training)
+
+        # Followed by another Add & Norm layer
+        return self.add_norm3(addnorm_output2, feedforward_output)
+
+
+class Decoder(Layer):
+    def __init__(self, vocab_size, sequence_length, h, d_k, d_v, d_model, d_ff, n, rate, **kwargs):
+        super().__init__(**kwargs)
+        self.pos_encoding = PositionEmbeddingFixedWeights(sequence_length, vocab_size, d_model)
+        self.dropout = Dropout(rate)
+        self.decoder_layer = [DecoderLayer(h, d_k, d_v, d_model, d_ff, rate) for _ in range(n)]
+
+    def call(self, output_target, encoder_output, lookahead_mask, padding_mask, training):
+        # Generate the positional encoding
+        pos_encoding_output = self.pos_encoding(output_target)
+        # Expected output shape = (number of sentences, sequence_length, d_model)
+        # Add in a dropout layer
+        x = self.dropout(pos_encoding_output, training=training)
+        # Pass on the positional encoded values to each encoder layer
+        for i, layer in enumerate(self.decoder_layer):
+            x = layer(x, encoder_output, lookahead_mask, padding_mask, training)
+        return x
+
